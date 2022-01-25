7b29f946a52d8e8c3a2e084ec17f31639c97158d
eren.liao <eren.liao@shijigroup.com>
2022-01-25 10:49:29 +0800

    暂时不继承 AbstractTransactionalJUnit4SpringContextTests

--- springboot-autoTest/src/test/java/com/lzl/autoTest/base/SpringBootTestBase.java
+++ springboot-autoTest/src/test/java/com/lzl/autoTest/base/SpringBootTestBase.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class SpringBootTestBase extends AbstractTransactionalJUnit4SpringContextTests {
+public class SpringBootTestBase {
 
     @Before
     public void before(){
