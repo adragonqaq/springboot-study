2e16d4edb111a35d0674abe5607cc1736adc3b18
eren.liao <eren.liao@shijigroup.com>
2022-01-25 10:47:51 +0800

    添加自动测试模块

---
+++ springboot-autoTest/src/main/java/com/lzl/autoTest/AutoTestApplication.java
@@ -0,0 +1,15 @@
+package com.lzl.autoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lzl
+ */
+@SpringBootApplication
+public class AutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestApplication.class, args);
+	}
+}
---
+++ springboot-autoTest/src/test/java/com/lzl/autoTest/AutoTest.java
@@ -0,0 +1,17 @@
+package com.lzl.autoTest;
+
+import com.lzl.autoTest.base.SpringBootTestBase;
+import org.junit.Test;
+
+/**
+ * @author eren.liao
+ * @date 2022/1/25 10:39
+ */
+public class AutoTest extends SpringBootTestBase {
+
+
+    @Test
+    public void createOrder(){
+        System.out.println("第一次测试");
+    }
+}
