b725528a730a055a631514ea4354c449cd1100ad
eren.liao <eren.liao@shijigroup.com>
2024-04-12 10:36:28 +0800

    springboot ai

---
+++ springboot-ai/src/main/java/com/lzl/ai/AiApplication.java
@@ -0,0 +1,15 @@
+package com.lzl.ai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class, args);
+    }
+
+}
