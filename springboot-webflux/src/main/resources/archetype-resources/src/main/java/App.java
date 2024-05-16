567a839a23dd5abba0fb755ac41df700b4320f1f
eren.liao <eren.liao@shijigroup.com>
2024-05-16 08:32:39 +0800

    提交代码

---
+++ springboot-webflux/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.lzl.webflux;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
