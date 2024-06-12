807ea8001980543c8578827edd5e965ee2cdb0be
eren.liao <862133778@qq.com>
2024-06-12 21:31:22 +0800

    初始化cloud项目

---
+++ springboot-cloud/feign-ribbon-hystrix-nacos/src/main/java/com/lzl/frhn/FRHNApplication.java
@@ -0,0 +1,13 @@
+package com.lzl.frhn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FRHNApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FRHNApplication.class, args);
+    }
+
+}
