2bf6e8080ef63479a9dd56f2e692bd30d38221dc
eren.liao <862133778@qq.com>
2024-06-15 13:08:34 +0800

    整合项目

--- springboot-mq/rabbitmq/src/test/java/com/lzl/rabbitmq/RabbitmqTest.java
+++ springboot-mq/rabbitmq/src/test/java/com/lzl/rabbitmq/RabbitmqTest.java
@@ -1,13 +1,12 @@
 package com.lzl.rabbitmq;
 
 import com.lzl.rabbitmq.producter.Producter;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class RabbitmqTest {
 
