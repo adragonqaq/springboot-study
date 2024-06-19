fad05a484ff31c48ceb0d56694a110a79a40547a
eren.liao <862133778@qq.com>
2024-06-19 20:49:15 +0800

    集成jwt

---
+++ springboot-security/oauth2/src/main/java/com/lzl/oauth2/App.java
@@ -0,0 +1,13 @@
+package com.lzl.oauth2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
