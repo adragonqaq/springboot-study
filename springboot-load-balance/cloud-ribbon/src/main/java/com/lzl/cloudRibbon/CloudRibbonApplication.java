2652c1d65332f89ea29ae08e52867a9b05f7dbd3
eren.liao <862133778@qq.com>
2024-06-25 09:03:30 +0800

    提交

---
+++ springboot-load-balance/cloud-ribbon/src/main/java/com/lzl/cloudRibbon/CloudRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.lzl.cloudRibbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudRibbonApplication.class, args);
+    }
+
+}
